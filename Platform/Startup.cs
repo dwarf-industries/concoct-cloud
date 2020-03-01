642ad55efbf8ea53a128b0a29859d3d232493007
root <root@pop-os.localdomain>
2020-03-01 15:35:42 +0200

    temporary commit

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -23,7 +23,7 @@ namespace Rokono_Control
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
