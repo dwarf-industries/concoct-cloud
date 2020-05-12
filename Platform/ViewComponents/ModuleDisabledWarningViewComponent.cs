7068e50f4501dbdf4c8a8133e97f404e0573ee78
dwarfdevelopment <dwarfdevelopment@pop-os.localdomain>
2020-05-12 13:53:21 +0300

    Adding validation to check if a public feature is activated and a notification message on the public page that says the feature is disabled.

---
+++ Platform/ViewComponents/ModuleDisabledWarningViewComponent.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Rokono_Control.Models;
+
+namespace Platform.ViewComponents
+{
+    public class ModuleDisabledWarningViewComponent : ViewComponent 
+    {
+           private readonly RokonoControlContext Context;
+        private readonly IConfiguration Configuration;
+
+        public ModuleDisabledWarningViewComponent(RokonoControlContext context, IConfiguration config)
+        {
+            Context = context;
+            Configuration = config;
+        }
+
+        public IViewComponentResult Invoke(int projectId)
+        { 
+            
+            return View();
+        }
+    }
+}
