a9f4b04bcbefa4563d23a8fb1e9f2d1390203236
dwarfdevelopment <support@rokonoconrol.com>
2020-08-09 20:23:32 +0300

    Adding ID generator in handles to generate random Ids for different dynamic syncfusion components used in the Survey Builder. Adding SurveyInput ViewComponent to load an input widget in the SurveyBuilder, implementing business logic to render the view component when the badge label is dropped inside the render field.

--- Platform/Controllers/SurveyController.cs
+++ Platform/Controllers/SurveyController.cs
@@ -1,6 +1,8 @@
 namespace Platform.Controllers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
@@ -38,7 +40,28 @@ namespace Platform.Controllers
                 ViewData["BuildingBlocks"] = context.GetSurveyComponents();
             return View();
         }
-
+        [HttpGet]
+        public async Task<IActionResult> GetProjectSurveyElement(string element)
+        { 
+            
+            var id = default(int);
+            if(element.Contains("_"))
+                int.TryParse(element.Split("_")[1], out id);
+            var componentName = string.Empty;
+            using(var context = new SurveyContext(Context,Config))
+            {
+                var component = context.GetSurveyComponents().FirstOrDefault(x=>x.Id == id);
+                componentName = component != null ? component.ComponentInternalName : ""; 
+            }
+            if(!string.IsNullOrEmpty(componentName))
+                return await Task.Run(() => ViewComponent(componentName, new IncomingIdRequest{
+                    
+                }));
+             
+            return await Task.Run(() => ViewComponent("NullComponent", new IncomingIdRequest{
+                
+            }));
+        }
         [HttpPost]
         public async Task<List<Surveys>> GetProjectSurveys([FromBody] IncomingIdRequest request)
         {
