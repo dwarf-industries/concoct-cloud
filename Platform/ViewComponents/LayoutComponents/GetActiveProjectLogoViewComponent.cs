8fc95d6618417ac5c3e3a7495d741e31ff349b2a
kristiformilchev <support@rokonoconrol.com>
2020-08-09 13:38:53 +0300

    Adding project specific LayoutDetails allowing users to define custom project name in the layout menu as well as add their own project logo to the main menu.

---
+++ Platform/ViewComponents/LayoutComponents/GetActiveProjectLogoViewComponent.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Rokono_Control.DatabaseHandlers;
+using Rokono_Control.Models;
+
+namespace Platform.ViewComponents.LayoutComponents
+{
+    [ViewComponent(Name = "GetProjectLogo")]
+    public class GetActiveProjectLogoViewComponent : ViewComponent
+    {
+        private readonly RokonoControlContext Context;
+        private readonly IConfiguration Configuration;
+
+        public GetActiveProjectLogoViewComponent(RokonoControlContext context, IConfiguration configuration)
+        {
+            Context = context;
+            Configuration = configuration;
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            using(var context = new DatabaseController(Context,Configuration))
+            {
+                var projectDetails = context.GetProjectData(id);
+                ViewData["ImageLocation"] = projectDetails.ImageLocation;
+            }
+            return View("/Views/Shared/Components/LayoutComponents/ProjectSpecific/ProjectImage.cshtml");
+        }
+    }
+}
---
+++ Platform/ViewComponents/LayoutComponents/GetActiveProjectNameViewComponent.cs
@@ -0,0 +1,27 @@
+namespace Platform.ViewComponents.LayoutComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Rokono_Control.DatabaseHandlers;
+    using Rokono_Control.Models;
+
+    [ViewComponent(Name = "GetProjectName")]
+    public class GetActiveProjectNameViewComponent : ViewComponent
+    {
+        private readonly RokonoControlContext Context;
+        private readonly IConfiguration Configuration;
+
+        public GetActiveProjectNameViewComponent(RokonoControlContext context, IConfiguration configuration)
+        {
+            Context = context;
+            Configuration = configuration;
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            using(var context = new DatabaseController(Context,Configuration))
+            ViewData["ProjectName"] = context.GetProjectName(id);
+            return View("/Views/Shared/Components/LayoutComponents/ProjectSpecific/ProjectName.cshtml");
+        }
+    }
+}
