7f659b961f401932989f445ea65eff32918e9e02
dwarfdevelopment <support@rokonoconrol.com>
2020-09-10 23:06:40 +0300

    Adding Survey Open Question functionality to the survey builder.

---
+++ Platform/ViewComponents/SurveyComponents/SurveyOpenQuestion/SurveyOpenQuestionViewComponent.cs
@@ -0,0 +1,35 @@
+
+
+namespace Platform.ViewComponents.SurveyComponents.SurveyOpenQuestion
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json;
+    using Platform.Models;
+    using Rokono_Control.Models;
+
+    [ViewComponent(Name = "SurveyOpenQuestion")]
+    public class SurveyOpenQuestionViewComponent : ViewComponent
+    {
+        private readonly RokonoControlContext Context;
+        private readonly IConfiguration Configuration;
+
+        public SurveyOpenQuestionViewComponent(RokonoControlContext context, IConfiguration configuration)
+        {
+            Context = context;
+            Configuration = configuration;
+        }
+        public IViewComponentResult Invoke(GenericIdRequest current)
+        {
+            ViewData["Id"] = current.Data[0];
+            ViewData["QuestionId"] = current.Data[1];
+            ViewData["ComponentId"] = current.Data[2];
+            if(current.Data[3] != null)
+                ViewData["DataSource"] = JsonConvert.DeserializeObject<BindingDynamicSource>(current.Data[3].ToString());
+            else
+                ViewData["DataSource"] = null;
+
+            return View("/Views/Shared/Components/Survey/OpenQuestion/Default.cshtml");
+        }
+    }
+}
