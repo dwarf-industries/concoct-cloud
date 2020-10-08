6c940047a7dc396210171a9c29be2ee39eb9d186
Kristifor Milchev <kristiformilchev615@gmail.com>
2020-10-08 05:29:29 +0300

    Removing obsolete Syncfusion library, moving the project to use cdns for easier development, for production its advised to get javascript es6 locally

--- Platform/wwwroot/Syncfusion/ej2/gantt.ts
+++
@@ -1,3 +0,0 @@
-import * as index from '@syncfusion/ej2-gantt';
-index.Gantt.Inject( index.Filter,index.Selection,index.Sort,index.Reorder,index.Resize,index.Edit,index.DayMarkers,index.Toolbar);
-export * from '@syncfusion/ej2-gantt';
