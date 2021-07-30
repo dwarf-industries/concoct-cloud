5a724f9c67ccfae3f7965d4c6a9de185c198c77e
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-07-30 08:27:19 +0300

    Adding error page to the solution so that users aren't forwarded to blank page each time something throws an exception

--- Startup.cs
+++ Startup.cs
@@ -77,6 +77,7 @@
               //  ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
 
+            app.UseExceptionHandler("/home/error");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
