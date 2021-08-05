cf60892e848ac3d3081d853ad634a2ab6c15acc2
Lyubomir Lambov <lambov122@gmail.com>
2021-08-05 12:54:06 +0300

    Fixed duplicating accounts bug

--- DatabaseHandlers/Contexts/UsersContext.cs
+++ DatabaseHandlers/Contexts/UsersContext.cs
@@ -159,15 +159,19 @@ namespace Platform.DatabaseHandlers.Contexts
                                                                         && x.UserAccountId == account.Id) != null ? true : false;
         }
 
-        internal bool CheckProjectSignUpPolicy(int projectId)
+        internal  (bool,bool) CheckProjectSignUpPolicy(int projectId, string email)
         {
             var exist = Context.Projects.FirstOrDefault(x => x.Id == projectId);
-
+            var accountName = Context.UserAccounts.FirstOrDefault(x => x.Email == email);
+            if (accountName != null)
+                return (false,true);
             if (exist == null)
-                return false;
+                return (false,false);
 
-            return exist.AllowPublicControl != null && exist.AllowPublicControl == 1 ? true : false;
+            return (exist.AllowPublicControl != null && exist.AllowPublicControl == 1 ? true : false,false);
         }
+
+            
 
         internal bool CheckAccountExist(string phase)
         {
