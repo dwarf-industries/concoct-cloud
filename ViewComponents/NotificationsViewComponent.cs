d03451b8ef4c38422e4751ea7503bb694199394f
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-08-10 19:24:17 +0300

    Moving Notification to an interface that an injecting it to all layouts so that alerts can be accessable across the solution.

---
+++ ViewComponents/NotificationsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿ namespace Rokono_Control.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class NotificationsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
