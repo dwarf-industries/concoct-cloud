a97f235b41c8eacf8f09a16ed8f58399929ca2e7
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-08-29 12:12:37 +0300

    Implementing a hotfix, preventing the email module to start as appsettings.development.json is ignored after publish on runtime.

--- Startup.cs
+++ Startup.cs
@@ -27,13 +27,14 @@
         }
         public StartupConfig StartConfiguration {get; set;}
         public IConfiguration Configuration;
-
+        public static StartupConfig Config { get; set; }
  
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             var fileData = File.ReadAllText("appsettings.Development.json");
             StartConfiguration = JsonConvert.DeserializeObject<StartupConfig>(fileData);
+            Config = StartConfiguration;
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
